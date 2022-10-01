a51b72b3f6a76567200cf64ef88611a7abd1d0f1
David <saghatelyandsm@gmail.com>
2022-10-01 18:56:18 -0300

    Agregamos nilo-pagina

---
+++ src/componentes/productoAnillo.js
@@ -0,0 +1,19 @@
+import React from 'react'
+//'../imagenes/anilloInfinito.png'
+function Anillo(){
+    return (
+    <div className='contenedor-anillo'>
+        <img 
+        className='imagen-anillo'
+        src={require('../imagenes/anilloInfinito.png')}
+        alt={'Anillo Infinito' }/>
+        <div className='propsAnillo'>
+        <p>Precio: 500</p>
+        </div>
+    </div>)
+}
+
+
+//let Infinito = Anillo('../imagenes/anilloInfinito.png', 500, Infinito)
+
+export default Anillo;
